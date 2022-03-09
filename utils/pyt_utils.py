c04aa8c89c6472ee223ecdc7b98a47ec13495e35
Haiduongcable <haiduongqweasd@gmail.com>
2022-03-09 22:02:44 +0700

    Update model

--- utils/pyt_utils.py
+++ utils/pyt_utils.py
@@ -50,6 +50,9 @@ def load_model(model, model_file, is_restore=False):
     own_keys = set(model.state_dict().keys())
     missing_keys = own_keys - ckpt_keys
     unexpected_keys = ckpt_keys - own_keys
+    
+    print(missing_keys)
+    print(unexpected_keys)
 
     del state_dict
     t_end = time.time()
